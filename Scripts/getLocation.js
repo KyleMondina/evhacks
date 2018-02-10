b5f16b082710ee478dc2240f75e45214bc0fd241
Kyle Mondina <kylemondina@gmail.com>
2018-02-10 21:22:26 +0000

    testing ajax codes

---
+++ Scripts/getLocation.js
@@ -0,0 +1,31 @@
+/*global $*/
+$(document).ready(function(){
+    var userData = {
+        userLat : "",
+        userLong : ""
+    };
+    
+    $('form').on('submit', function(){
+       
+        userData.userLat = $("input[id='lat']").val();
+        userData.userLong = $("input[id='long']").val();
+        alert('hi');
+        alert(userData.userLat);
+        alert(userData.userLat);
+        
+         $.ajax({
+          type: 'POST',
+          url: '/',
+          data: userData,
+          success: function(data){
+              console.log(data);
+            alert(data);
+            alert("connection successful");
+            //window.location.replace("getLocation");
+          },
+        });
+        
+        
+      
+    });
+});
--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const app = express();
 const bodyParser = require('body-parser');
 const urlencodedparser = bodyParser.urlencoded({extended:false});
 
+var userInfo =
+{
+  userLat:"",
+  userLong:""
+  
+}
+
 app.set('view engine', 'ejs');
 app.use('/Assets', express.static('Assets'));
 app.use('/Scripts', express.static('Scripts'));
@@ -11,5 +18,16 @@ app.use('/node_modules', express.static('node_modules'));
 app.get('/', function(req,res){
   res.render('index');
 });
+app.post('/', urlencodedparser, function(req,res)
+{
+  const userInput = req.body;
+  console.log('hi');
+  res.json({hi:"hi"});
+});
+app.get('/demographicDisplay', function(req,res){
+    
+  res.render('demographicDisplay');
+});
+
 
 app.listen(process.env.PORT);
