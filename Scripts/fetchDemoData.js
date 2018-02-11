73c39278cba0d161f95410edbc3e321a15cd2a26
KyleMondina <kylemondina@gmail.com>
2018-02-10 16:05:28 -0800

    debugged api

--- Scripts/fetchDemoData.js
+++ Scripts/fetchDemoData.js
@@ -1,11 +1,17 @@
 $(document).ready(function(){
 
-    var token = "7X57HEIaLekNE56gk-9p3vAJBBk6oFLb6saVp4xUgmuHBRFargCA2aoHCwn29lfHC34o3pJz2h489-8yhjZXNTGXhyNsx0iYFpAXdQIFhOtfz-nAje6JANBEUPg3I1kTKqTmTnIXOS5DAzZnzbgceA..";
-    const testlat = 33.81091;
-    const testlong = -118.09047;
+    var token = "tkJPdjkFtqdlprW9wX_uPH467_tAz9-YTuwBMaoRrTim2QcxZSize5z8wz0kD5pWzLpUjRkfeVaFfSjPD0jG8Iu__fyg2DpVGQQZ85UKJm-9QFIVk8QVhTkDbJjkWLf1_EpMzf3GK3BQ_pfKaMRoyw..";
+    const testlat = -118.09047;
+    const testlong = 33.81091;
 
     const sanjose = [37.338208,-121.886329];
     var demoData;
+
+    var totalPop;
+    var totalHouseHolds;
+    var avgHomeVal;
+    var popGrowthYear2017_2022;
+    var houseGrowthYear2017_2022;
 
 
     alert('hi');
@@ -28,18 +34,18 @@ $(document).ready(function(){
     var settings = {
     "async": true,
     "crossDomain": true,
-    "url": `https://geoenrich.arcgis.com/arcgis/rest/services/World/geoenrichmentserver/GeoEnrichment/enrich?f=json&token=7X57HEIaLekNE56gk-9p3vAJBBk6oFLb6saVp4xUgmuHBRFargCA2aoHCwn29lfHC34o3pJz2h489-8yhjZXNTGXhyNsx0iYFpAXdQIFhOtfz-nAje6JANBEUPg3I1kTKqTmTnIXOS5DAzZnzbgceA..&inSR=4326&outSR=4326&returnGeometry=true&studyAreas=[%0A%20%20{%0A%20%20%20%20%22geometry%22:{%0A%20%20%20%20%20%20%22x%22:-118.09047,%0A%20%20%20%20%20%20%22y%22:33.81091%0A%20%20%20%20}%0A%20%20}%0A]&studyAreasOptions={%0A%20%20%22areaType%22:%22RingBuffer%22,%0A%20%20%22bufferUnits%22:%22esriMiles%22,%0A%20%20%22bufferRadii%22:[1]%0A}&dataCollections=[%22KeyGlobalFacts%22,%20%22KeyUSFacts%22]`,
+    "url": `https://geoenrich.arcgis.com/arcgis/rest/services/World/geoenrichmentserver/GeoEnrichment/enrich?f=json&token=${token}&inSR=4326&outSR=4326&returnGeometry=true&studyAreas=[%0A%20%20{%0A%20%20%20%20%22geometry%22:{%0A%20%20%20%20%20%20%22x%22:${testlat},%0A%20%20%20%20%20%20%22y%22:${testlong}%0A%20%20%20%20}%0A%20%20}%0A]&studyAreasOptions={%0A%20%20%22areaType%22:%22RingBuffer%22,%0A%20%20%22bufferUnits%22:%22esriMiles%22,%0A%20%20%22bufferRadii%22:[1]%0A}&dataCollections=[%22KeyGlobalFacts%22,%20%22KeyUSFacts%22]`,
     "method": "POST",
     "headers": {
       //"Cache-Control": "no-cache",
-      //"Postman-Token": "4a368af8-c3b2-cf9e-36d6-3ce5ef57354c"
+      //"Postman-Token": "2fa5c575-4c02-8aa9-deab-e6364d9557ea"
     }
-  }
+    }
 
-  $.ajax(settings).done(function (response) {
-    demoData = JSON.parse(response)
+    $.ajax(settings).done(function (response) {
+    demoData = JSON.parse(response);
     console.log(demoData);
+    });
   });
-  })
 
 });
